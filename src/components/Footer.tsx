50d7deec60f63990a63c4841c9ce6dfa4c674ef8
Sergi Bosque Ródenas <IDSBR0@idneo.local>
2025-03-17 20:29:18 +0100

    color dark mode icons

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -16,29 +16,40 @@ const links: Link[] = [
     link: "/",
     text: "Github",
   },
-]
+];
 
 const Footer = () => {
-
   return (
     <footer className="flex flex-row items-center gap-4 py-8 px-5 border-t-1 border-secondary-300 dark:border-secondary-dark-300 mt-12 text-sm">
-      <h3 className="text-secondary-400 dark:text-secondary-dark-400 text-sm uppercase tracking-widest">Contact me</h3>
+      <h3 className="text-secondary-400 dark:text-secondary-dark-400 text-sm uppercase tracking-widest">
+        Contact me
+      </h3>
       <ul className="flex flex-wrap gap-4">
         {links.map((link) => {
           return (
-            <li className="flex items-center gap-2 hover:underline" key={link.text}>
-              <svg xmlns="http://www.w3.org/2000/svg" width="11" height="10" viewBox="0 0 11 10" fill="none">
-                <path d="M10 9.1087V0.5M10 0.5H1.3913M10 0.5L1 9.5" stroke="#333333" />
+            <li
+              className="flex items-center gap-2 hover:underline"
+              key={link.text}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="11"
+                height="10"
+                viewBox="0 0 11 10"
+                fill="none"
+              >
+                <path
+                  d="M10 9.1087V0.5M10 0.5H1.3913M10 0.5L1 9.5"
+                  stroke="currentColor"
+                />
               </svg>
               <a href={link.link}>{link.text}</a>
             </li>
-          )
+          );
         })}
       </ul>
-
-
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
--- src/components/ProjectCardItem.tsx
+++ src/components/ProjectCardItem.tsx
@@ -2,28 +2,43 @@ import { NavLink } from "react-router-dom";
 import { Project } from "../models/project.interface";
 
 interface ProjectCardItemProps {
-    project: Project;
+  project: Project;
 }
 
 const ProjectCardItem: React.FC<ProjectCardItemProps> = ({ project }) => {
-    return (
-        <NavLink to={`/projects/${project.id}`}>
-            <li className="group">
-                <div className="w-full h-auto overflow-hidden">
-                    <img src={project.imageUrl} alt={project.title} className="w-full h-auto transition-transform duration-400 ease-in-out group-hover:scale-110" />
-                </div>
-                <div className="flex pr-6 py-6 justify-between border-b-1 transition-all duration-400 ease-in-out group-hover:pr-2">
-                    <div className="flex flex-col self-stretch gap-1">
-                        <p className="text-secondary-400 dark:text-secondary-dark-400 text-xs">{project.discipline} | {project.year}</p>
-                        <h2 className="text-2xl">{project.title}</h2>
-                    </div>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 48 48" fill="none">
-                        <path d="M2 23.5H46M46 23.5L36.9091 14M46 23.5L36.9091 33" stroke="#333333" />
-                    </svg>
-                </div>
-            </li>
-        </NavLink>
-    )
-}
+  return (
+    <NavLink to={`/projects/${project.id}`}>
+      <li className="group">
+        <div className="w-full h-auto overflow-hidden">
+          <img
+            src={project.imageUrl}
+            alt={project.title}
+            className="w-full h-auto transition-transform duration-400 ease-in-out group-hover:scale-110"
+          />
+        </div>
+        <div className="flex pr-6 py-6 justify-between border-b-1 transition-all duration-400 ease-in-out group-hover:pr-2">
+          <div className="flex flex-col self-stretch gap-1">
+            <p className="text-secondary-400 dark:text-secondary-dark-400 text-xs">
+              {project.discipline} | {project.year}
+            </p>
+            <h2 className="text-2xl">{project.title}</h2>
+          </div>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="48"
+            height="48"
+            viewBox="0 0 48 48"
+            fill="none"
+          >
+            <path
+              d="M2 23.5H46M46 23.5L36.9091 14M46 23.5L36.9091 33"
+              stroke="currentColor"
+            />
+          </svg>
+        </div>
+      </li>
+    </NavLink>
+  );
+};
 
-export default ProjectCardItem
+export default ProjectCardItem;
