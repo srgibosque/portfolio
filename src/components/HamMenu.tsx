ba71a8471c1f516ba91c4cc5849a83d810ba7bfb
Sergi Bosque Ródenas <IDSBR0@idneo.local>
2025-03-27 10:20:24 +0100

    hamMenu animation

--- src/components/HamMenu.tsx
+++ src/components/HamMenu.tsx
@@ -1,15 +1,63 @@
-interface HamMenuProps {
-    onClick: () => void;
-  }
+import { useEffect, useState } from "react";
+import { motion } from "framer-motion";
 
-const HamMenu: React.FC<HamMenuProps> = ({onClick}) => {
-  return (
-    <button onClick={onClick}  className="flex flex-col gap-2 md:hidden">
-      <span className="bg-primary dark:bg-white block w-6 h-[1px]"></span>
-      <span className="bg-primary dark:bg-white block w-6 h-[1px]"></span>
-      <span className="bg-primary dark:bg-white block w-6 h-[1px]"></span>
-    </button>
-  );
+interface HamMenuProps {
+  onClick: () => void;
+}
+
+const HamMenu: React.FC<HamMenuProps> = ({ onClick }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+    onClick();
+  };
+
+   useEffect(() => {
+      const handleResize = () => {
+        if (window.innerWidth >= 768) {
+          setIsOpen(false); // Close menu when hitting md breakpoint
+        }
+      };
+  
+      window.addEventListener("resize", handleResize);
+  
+      return () => {
+        window.removeEventListener("resize", handleResize);
+      };
+    }, [isOpen]);
+
+    return (
+      <button
+        onClick={handleClick}
+        className="md:hidden relative w-[36px] h-[20px]"
+      >
+        <motion.span
+          className="absolute left-0 h-[1px] w-full bg-primary dark:bg-primary-dark"
+          animate={{
+            top: isOpen ? "50%" : "0%",
+            rotate: isOpen ? 135 : 0,
+          }}
+          transition={{ duration: 0.25, ease: "easeInOut" }}
+        />
+        <motion.span
+          className="absolute left-0 h-[1px] w-full bg-primary dark:bg-primary-dark"
+          animate={{
+            opacity: isOpen ? 0 : 1,
+            x: isOpen ? -50 : 0,
+          }}
+          transition={{ duration: 0.25, ease: "easeInOut" }}
+        />
+        <motion.span
+          className="absolute left-0 h-[1px] w-full bg-primary dark:bg-primary-dark"
+          animate={{
+            top: isOpen ? "50%" : "100%",
+            rotate: isOpen ? -135 : 0,
+          }}
+          transition={{ duration: 0.25, ease: "easeInOut" }}
+        />
+      </button>
+    );
 };
 
 export default HamMenu;
